928ac1390bf5aed6437ff4228188e76c7b8a6066
Marco Roccon <marco.roccon@gmail.com>
2025-06-22 14:12:48 -0400

    Integrated Supra library for pulling data feeds, implementation to be finished

---
+++ oracle/main.js
@@ -0,0 +1,108 @@
+require("dotenv").config();
+const PullServiceClient = require("./pullServiceClient");
+const { Web3 } = require("web3");
+
+async function main() {
+  const address = "";
+  const pairIndexes = [0, 1, 10]; // BTC/USDT, ETH/USDT, SOL/USDT
+  const chainType = "evm";
+
+  const client = new PullServiceClient(address);
+
+  const request = {
+    pair_indexes: pairIndexes,
+    chain_type: chainType,
+  };
+
+  console.log("Requesting proof for price index : ", request.pair_indexes);
+  client.getProof(request, (err, response) => {
+    if (err) {
+      console.error("Error:", err.details);
+      return;
+    }
+    console.log("Calling contract to verify the proofs...");
+    callContract(response.evm);
+  });
+
+  async function callContract(response) {
+    const web3 = new Web3(new Web3.providers.HttpProvider(""));
+    const contractAbi = require("./resources/abi.json");
+    const contractAddress = "";
+    const contract = new web3.eth.Contract(contractAbi, contractAddress);
+    const hex = web3.utils.bytesToHex(response.proof_bytes);
+
+    //////////// Utility code for deserialising oracle proof bytes ////////////
+
+    const OracleProofABI = require("./resources/oracleProof.json");
+
+    let proof_data = web3.eth.abi.decodeParameters(OracleProofABI, hex);
+
+    let pairId = [];
+    let pairPrice = [];
+    let pairDecimal = [];
+    let pairTimestamp = [];
+
+    for (let i = 0; i < proof_data[0].data.length; ++i) {
+      for (
+        let j = 0;
+        j < proof_data[0].data[i].committee_data.committee_feed.length;
+        j++
+      ) {
+        pairId.push(
+          proof_data[0].data[i].committee_data.committee_feed[j].pair.toString(
+            10
+          )
+        ); // pushing the pair ids requested in the output vector
+        pairPrice.push(
+          proof_data[0].data[i].committee_data.committee_feed[j].price.toString(
+            10
+          )
+        ); // pushing the pair price for the corresponding ids
+        pairDecimal.push(
+          proof_data[0].data[i].committee_data.committee_feed[
+            j
+          ].decimals.toString(10)
+        ); // pushing the pair decimals for the corresponding ids requested
+        pairTimestamp.push(
+          proof_data[0].data[i].committee_data.committee_feed[
+            j
+          ].timestamp.toString(10)
+        ); // pushing the pair timestamp for the corresponding ids requested
+      }
+    }
+
+    console.log("Pair index : ", pairId);
+    console.log("Pair Price : ", pairPrice);
+    console.log("Pair Decimal : ", pairDecimal);
+    console.log("Pair Timestamp : ", pairTimestamp);
+
+    /////////// End of code for deserialising oracle proof bytes ////////////
+
+    const txData = contract.methods.verifyOracleProof(hex).encodeABI();
+    const gasEstimate = await contract.methods
+      .verifyOracleProof(hex)
+      .estimateGas({ from: "" });
+
+    const transactionObject = {
+      from: "",
+      to: contractAddress,
+      data: txData,
+      gas: gasEstimate,
+      gasPrice: await web3.eth.getGasPrice(),
+    };
+
+    const signedTransaction = await web3.eth.accounts.signTransaction(
+      transactionObject,
+      ""
+    );
+
+    const receipt = await web3.eth.sendSignedTransaction(
+      signedTransaction.rawTransaction,
+      null,
+      { checkRevertBeforeSending: false }
+    );
+    console.log("Transaction receipt:", receipt);
+  }
+}
+
+main();
