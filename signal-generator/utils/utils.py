b2d7392d354af2540f1b8b54baffc180d8dcb47c
Marco Roccon <marco.roccon@gmail.com>
2025-06-22 17:43:44 -0400

    Unified signal generation logic in a single call, looped every 5 minutes

--- signal-generator/utils/utils.py
+++ signal-generator/utils/utils.py
@@ -1,3 +1,16 @@
+import os
+import ta
+import pandas as pd
+from pymongo import MongoClient
+from dotenv import find_dotenv, load_dotenv
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+client = MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
+db = client["oracle"]
+collection = db["price_feeds"]
+
 def get_signal_rsi(x):
     if x is None:
         return 0
@@ -7,3 +20,19 @@ def get_signal_rsi(x):
         return 1
     else:
         return 0
+    
+def process_pair(pair: str):
+    cursor = collection.find({"pair": pair}).sort("timestamp", 1)
+    docs = list(cursor)
+    df = pd.DataFrame(docs)
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    
+    df = df.sort_values(by="timestamp").reset_index(drop=True)
+    df = df.resample("5min", on="timestamp").agg({
+        "timestamp": "last",
+        "pair": "first",
+        "price": "last"
+    }).dropna().reset_index()
+    
+    df["rsi"] = ta.momentum.RSIIndicator(df["price"], window=14).rsi()
+    df["rsi_signal"] = df["rsi"].apply(get_signal_rsi)
