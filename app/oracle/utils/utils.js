7e9b661fc1a75e7bf8d92416826c3b830c4c131e
Paolo Antonio Rossi <paolo@luduvigo.com>
2025-06-23 11:53:55 -0400

    update app

--- app/oracle/utils/utils.js
+++ app/oracle/utils/utils.js
@@ -1,8 +1,10 @@
-export function getSymbolFromId(id) {
+function getSymbolFromId(id) {
   const map = {
-    "0": "BTC/USDT",
-    "1": "ETH/USDT",
-    "10": "SOL/USDT",
+    0: "BTC/USDT",
+    1: "ETH/USDT",
+    10: "SOL/USDT",
   };
   return map[id] || `UNKNOWN(${id})`;
 }
+
+module.exports = { getSymbolFromId };
